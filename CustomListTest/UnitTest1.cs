64f9a62edb9b4a2ae7850baed8e592339cde472c
Brandies7 <adanb82@gmail.com>
2017-08-17 12:51:23 -0500

    initial commmit

---
+++ CustomListTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FirstName<string> firstName = new FirstName<string>();
+            firstName.Add("Nico");
+            firstName.Add("Autumn");
+            firstName.Add("Bailey");
+            firstName.Add("Summer");
+            firstName.Add("Mason");
+
+        }
+    }
+}
