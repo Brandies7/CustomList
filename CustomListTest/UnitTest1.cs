b13dd76d4983a0be5fba64601ab559cf2a479155
Brandies7 <adanb82@gmail.com>
2017-08-17 13:23:18 -0500

    added five test methods

--- CustomListTest/UnitTest1.cs
+++ CustomListTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
 
 namespace CustomListTest
 {
@@ -9,13 +10,135 @@ namespace CustomListTest
         [TestMethod]
         public void TestMethod1()
         {
-            FirstName<string> firstName = new FirstName<string>();
+            CustomList<string> firstName = new CustomList<string>();
             firstName.Add("Nico");
             firstName.Add("Autumn");
             firstName.Add("Bailey");
             firstName.Add("Summer");
             firstName.Add("Mason");
+            string expectedResult = ("Nico");
+            string actualResult;
 
+            actualResult = [0];
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+
+        public void TestMethod2()
+        {
+            CustomList<string> firstName = new CustomList<string>();
+            firstName.Add("Nico");
+            firstName.Add("Autumn");
+            firstName.Add("Bailey");
+            firstName.Add("Summer");
+            firstName.Add("Mason");
+            string expectedResult = ("Autumn");
+            string actualResult;
+
+            actualResult = [1];
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+
+        public void TestMethod3()
+        {
+            CustomList<string> firstName = new CustomList<string>();
+            firstName.Add("Nico");
+            firstName.Add("Autumn");
+            firstName.Add("Bailey");
+            firstName.Add("Summer");
+            firstName.Add("Mason");
+            string expectedResult = ("Bailey");
+            string actualResult;
+
+            actualResult = [2];
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+
+        public void TestMethod4()
+        {
+            CustomList<string> firstName = new CustomList<string>();
+            firstName.Add("Nico");
+            firstName.Add("Autumn");
+            firstName.Add("Bailey");
+            firstName.Add("Summer");
+            firstName.Add("Mason");
+            firstName.Remove("Nico");
+            string expectedResult = ("Autumn");
+            string actualResult;
+
+            actualResult = [0];
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+
+        public void TestMethod5()
+        {
+            CustomList<string> firstName = new CustomList<string>();
+            firstName.Add("Nico");
+            firstName.Add("Autumn");
+            firstName.Add("Bailey");
+            firstName.Add("Summer");
+            firstName.Add("Mason");
+            firstName.Remove("Autumn");
+            string expectedResult = ("Bailey");
+            string actualResult;
+
+            actualResult = [1];
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+
+        public void TestMethod5()
+        {
+            CustomList<string> firstName = new CustomList<string>();
+            firstName.Add("Nico");
+            firstName.Add("Autumn");
+            firstName.Add("Bailey");
+            firstName.Add("Summer");
+            firstName.Add("Mason");
+            firstName.Remove("Summer");
+            string expectedResult = ("Mason");
+            string actualResult;
+
+            actualResult = [3];
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+
+        public void TestMethod5()
+        {
+            CustomList<string> firstName = new CustomList<string>();
+            firstName.Add("Nico");
+            firstName.Add("Autumn");
+            firstName.Add("Bailey");
+            firstName.Add("Summer");
+            firstName.Add("Mason");
+            firstName.Remove("Summer");
+            string expectedResult = ("Mason");
+            string actualResult;
+
+            actualResult = [3];
+
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
+
+
+
+
+ 
